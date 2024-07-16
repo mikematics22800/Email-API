4036662cd816cd5c284151c7233b5493ea0f3bad
Michael Medina <michaeljmedina22800@gmail.com>
2024-07-16 19:54:22 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# CLI-READ-ME-Generator
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+## Description
+A node application that manages and displays client information with backend CRUD operations.
+## Installation
+```
+cd $directory
+git clone https://github.com/mikemedina22800/Express-Note-Taker
+cd Express-Note-Taker
+npm i 
+```
+## Usage
+After installation, run `node server.js` in your terminal to host the app on a local server. Then interact with the UI to create and delete notes.
+
+## Contributing
+Contributions are welcome! For major changes, please open an issue first to discuss what you would like to change. Please make sure to update tests as appropriate.
+
+## Tests
+To run tests, install any necessary dependencies and run `npm test` in your terminal. Ensure you have a testing framework like Jest installed to execute the tests.
+
+## License
+[![alt text](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+## Questions
+Check my [Github Page](https://github.com/mikemedina22800) or email me at michaeljmedina22800@gmail.com.
---
+++ server.js
@@ -0,0 +1,51 @@
+// Import necessary modules
+import express from 'express';
+import cors from 'cors';
+import nodemailer from 'nodemailer';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://mikemedina22800.github.io'],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.static('public'));
+
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+
+app.post('/', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: email,
+      pass: password,
+    }
+  });
+
+  const mailOptions = {
+    from: email, 
+    to: 'mikematics@mikematics.net', 
+    subject: `Message from ${req.body.email}`,
+    text: req.body.message
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      res.send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Email sent successfully');
+    }
+  });
+});
+
+// Listen on the specified port
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
